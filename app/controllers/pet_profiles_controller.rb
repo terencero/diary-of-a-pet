20f3293c34f36271d5ace800fff7a7ecb9f3d5c4
Terence Ro <rowyourboat83@gmail.com>
2025-10-25 16:44:48 -0500

    add pet_profile edit page and update action

--- app/controllers/pet_profiles_controller.rb
+++ app/controllers/pet_profiles_controller.rb
@@ -1,5 +1,8 @@
 class PetProfilesController < ApplicationController
+  before_action :set_pet_profile, only: %i[ show edit update ]
+
   include ItemsByPet
+
   def index
     @pets = PetProfile.all
   end
@@ -10,5 +13,29 @@ class PetProfilesController < ApplicationController
     @tasks = get_items_sorted_by_pet(:tasks)
     @supplies = get_items_sorted_by_pet(:supplies)
     @appointments = get_items_sorted_by_pet(:appointments)
+  end
+
+  def edit
+    @pet = PetProfile.find(params[:id])
+  end
+
+  def update
+    if @pet.update(pet_profile_params)
+      redirect_to pet_profiles_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def pet_profile_params
+    params.expect(pet_profile: [ :animal, :name, :dob, :gender ])
+  end
+
+  def set_pet_profile
+    @pet = PetProfile.find(params[:id])
   end
 end
